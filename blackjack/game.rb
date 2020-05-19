925d4ac00afc17be0a084bb7ac7ca134c27ffb43
Dmitriy-Filatov <zakon@uma.su>
2020-05-20 01:33:15 +0300

    some new logic

--- blackjack/game.rb
+++ blackjack/game.rb
@@ -1,64 +1,141 @@
 # frozen_string_literal: true
 
 class Interface
-  puts 'Введите имя'
-  name = gets.chomp
-  Game.start unless !name.nil?
 end
 
 class Game
+  attr_reader :game_bank
+
+  def initialize
+    @interface = Interface.new
+    @game_bank = 0
+  end
+
+  def take_bet
+    @game_bank += 20
+  end
+
   def start
-    user = User.new
+    game = Game.new
+    puts 'Введите имя'
+    user = User.new(name: gets.chomp)
     diller = Diller.new
     deck = Deck.new
     user.get_cards(deck.pop_two_cards)
     diller.get_cards(deck.pop_two_cards)
+    puts(user.hand.cards.map(&:symbol))
+    puts(user.hand.score)
+    puts '**'
+    user.make_bet
+    diller.make_bet
+    game.take_bet
+    puts 'Введите абабаб'
+    detect_user_input(gets.chomp)
+    dillers_move(deck: deck, diller: diller)
+    check_cards_count(diller, user)
+  end
+
+  def detect_user_input(menu_number)
+    case menu_number
+    when '1'
+      nil
+    when '2'
+      add_one_card
+    when '3'
+      open_cards
+    end
+  end
+
+  def dillers_move(diller:, deck:)
+    if diller.hand.score >= 17
+      nil
+    else
+      diller.get_cards(deck.pop_card)
+    end
+  end
+
+  def add_one_card
+
+  end
+
+  def check_cards_count(diller, user)
+
+  end
+
+  def open_cards
+    game_over
+  end
+
+  def game_over
+    # Подсчет результатов
+    # Хочешь сыграть? да раздача нет выход
   end
 end
 
 class Player
-  attr_reader :name, :bank
+  attr_reader :player_bank, :hand
 
-  def initialize(name)
-    @name = name
+  def initialize
     @hand = Hand.new
-    @bank = 100
+    @player_bank = 100
   end
 
   def get_cards(cards)
     @hand.take_cards(cards)
   end
+
+  def make_bet
+    @player_bank -= 10
+  end
 end
 
 class User < Player
+  def initialize(name:)
+    @name = name
+    super()
+  end
 end
 
 class Diller < Player
+  def initialize(name: 'Diller')
+    @name = name
+    super()
+  end
 end
 
 class Hand
+  attr_reader :cards
+
   def initialize
-    @hand = []
+    @cards = []
   end
 
   def take_cards(cards)
     cards.each do |card|
-      @hand << card
+      @cards << card
     end
   end
 
-  def weight
-    return 11 if card_value == 'A'
+  def weight(card)
+    return 11 if card.card_value == 'A'
 
-    if ('2'..'10').include?(card_value)
-      card_value.to_i
+    if ('2'..'10').include?(card.card_value)
+      card.card_value.to_i
     else
       10
+    end
+  end
+
+  def score
+    @cards.sum do |card|
+      weight(card)
     end
   end
 end
 
 class Card
+  attr_reader :symbol
+
   def initialize(symbol)
     @symbol = symbol
   end
