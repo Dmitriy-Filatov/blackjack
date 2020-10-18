c66ffadbd5b9b1cb23867a02d680f6237d2752e6
Dmitriy-Filatov <zakon@uma.su>
2020-10-19 00:36:23 +0300

    RANKS SUITS

--- blackjack/card.rb
+++ blackjack/card.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
 
-class Card
-  attr_reader :symbol
+# class Cards
+#   attr_reader :symbol
+#
+#   SUITS = %w[].freeze
+#   VALUES = %w[].freeze
+#
+#   def initialize(symbol)
+#     @symbol = symbol
+#   end
+#
+#   def card_value
+#     @symbol[0]
+#   end
+# end
 
-  def initialize(symbol)
-    @symbol = symbol
+
+class Card
+  attr_reader :suit, :value
+
+  SUITS = %w[♣ ♦ ♥ ♠].freeze
+  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
   end
 
-  def card_value
-    @symbol[0]
+  def show
+    [@suit, @value]
   end
 end
