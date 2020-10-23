64719023af39f961b7e67beeaad86747da7c764b
Dmitriy-Filatov <zakon@uma.su>
2020-10-23 21:49:27 +0300

    Card

--- blackjack/card.rb
+++ blackjack/card.rb
@@ -1,21 +1,5 @@
 # frozen_string_literal: true
 
-# class Cards
-#   attr_reader :symbol
-#
-#   SUITS = %w[].freeze
-#   VALUES = %w[].freeze
-#
-#   def initialize(symbol)
-#     @symbol = symbol
-#   end
-#
-#   def card_value
-#     @symbol[0]
-#   end
-# end
-
-
 class Card
   attr_reader :suit, :value
 
