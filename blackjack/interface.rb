355076c6df112c9d6343e05f1e5e552aae91705c
Dmitriy-Filatov <zakon@uma.su>
2020-12-19 00:14:19 +0300

    clean

--- blackjack/interface.rb
+++ blackjack/interface.rb
@@ -1,45 +1,95 @@
 # frozen_string_literal: true
 
 class Interface
-  def initialize
-    ask_name
+  def initialize(argument)
+    @argument = argument
   end
 
   def ask_name
+    user_input_message
+    username_input
+    user_greeting
+    line
+  end
+
+  def user_input_message
     puts 'Введите свое имя и нажмите Enter!'
-    name = gets.chomp
-    @user.name = name
-    puts "Привет, #{name}. Игра началась!"
+  end
+
+  def username_input
+    @name = gets.chomp
+  end
+
+  def user_greeting
+    puts "Привет, #{@name}. Игра началась!"
+  end
+
+  def line
     puts '___________________'
   end
 
-  def show_users_score
-    puts "Сумма твоих очков, #{@user.name}: #{@user.hand.score}"
+  def user_cards
+    puts @user.hand.cards.map(&:face)
   end
 
-  def show_dealers_score
+  def dealers_invisible_cards
     puts 'Карты дилера |*| |*|'
   end
 
-  def show_horizontal_line
-    puts '___________________'
+  def dealer_cards
+    puts @dealer.hand.cards.map(&:face)
   end
 
-  def print_menu
+  def user_score
+    puts "Сумма твоих очков, #{@user.name}: #{@user.hand.score}"
+  end
+
+  def dealer_score
+    puts "Сумма очков дилера: #{@dealer.hand.score}"
+  end
+
+  def menu
     puts 'Введите: 1 - пропустить ход; 2 - добавить карту; 3 - открыть карты.'
   end
 
-  def party_loop
-    loop do
-      print_menu
-      menue_number = gets.chomp
-      show_cards
-      break if results
+  def prayers_cards_and_score
+    line
+    user_cards
+    user_score
+    line
+    dealer_cards
+    dealer_score
+    line
+  end
 
-      user_move(menue_number)
-      dealers_move
-      puts "Сумма твоих очков, #{@user.name}: #{@user.hand.score}"
+  def results
+    players_score
+    if @dealer_score == @user_score
+      dead_heat_message
+      return
     end
+    if @dealer_score < @user_score
+      user_winning_message
+    else
+      dealer_winning_message
+    end
+  end
+
+  def players_score
+    @dealer_score = (21 - @dealer.hand.score).abs
+    @user_score = (21 - @user.hand.score).abs
+  end
+
+  def dead_heat_message
+    puts 'Dead heat!'
+  end
+
+  def user_winning_message
+    puts "#{@user.name} wins!"
+  end
+
+  def dealer_winning_message
+    puts 'Dealer wins!'
   end
 
   def user_move(menue_number)
@@ -51,38 +101,6 @@ class Interface
       show_cards if @user.hand.cards.count == 3 && @dealer.hand.cards.count == 3
     when '3'
       show_cards
-    end
-  end
-
-  def one_card
-    @user.cards([@deck.pop_card])
-  end
-
-  def dealers_move
-    @dealer.cards([@deck.pop_card]) if @dealer.hand.score < 17
-  end
-
-  def show_cards
-    puts '___________________'
-    puts @user.hand.cards.map(&:face)
-    puts "Сумма твоих очков, #{@user.name}: #{@user.hand.score}"
-    puts '___________________'
-    puts @dealer.hand.cards.map(&:face)
-    puts "Сумма очков дилера: #{@dealer.hand.score}"
-    puts '___________________'
-  end
-
-  def results
-    dealer_score = (21 - @dealer.hand.score).abs
-    user_score = (21 - @user.hand.score).abs
-    if dealer_score == user_score
-      puts 'Dead heat!'
-      return
-    end
-    if dealer_score < user_score
-      puts "#{@user.name} lose. Dealer wins!"
-    else
-      puts "Dealer lose. #{@user.name} wins!"
     end
   end
 
